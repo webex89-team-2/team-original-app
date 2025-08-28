e2eccc09ef23f97f65a4b3cc188356f757e1155c
Kanta <kumicho441@gmail.com>
2025-08-28 19:34:57 +0900

    function_logIn

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { Link } from "react-router";
+import { Route, Routes } from "react-router-dom";
+// import App from "@/src/App.jsx";
+import Timeline from "@/src/pages/Timeline.jsx";
+import Search from "@/src/pages/Search.jsx";
+import Notification from "@/src/pages/Notification.jsx";
+import User from "@/src/pages/User.jsx";
+import Router from "./routes/route.jsx"
 
-function App() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>これは最初のページです。</p>
-      <Link to="/timeline">タイムライン</Link>
-    </div>
-  );
+export default function App(){
+  return <Router />;
 }
 
-export default App;
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<App />} />
+//         <Route path="/pages/SignIn" element={<SignIn />} />
+//         <Route path="/pages/SingUp" element={<SignUp />} />
+//         <Route path="/timeline" element={<Timeline />} />
+//         <Route path="/search" element={<Search />} />
+//         <Route path="/notification" element={<Notification />} />
+//         <Route path="/user/:id" element={<User />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,20 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-export { db, auth };
+ import { initializeApp } from "firebase/app";
+ import { getFirestore } from "firebase/firestore";
+ import { getAuth } from "firebase/auth";
+ 
+ // Firebaseコンソールからコピペする
+ const firebaseConfig = {
+  apiKey: "AIzaSyB_VbiT0GuMVlsjDUnCTTmKNYZIhT2PL8k",
+  authDomain: "webex-89-team2.firebaseapp.com",
+  projectId: "webex-89-team2",
+  storageBucket: "webex-89-team2.firebasestorage.app",
+  messagingSenderId: "687136905352",
+  appId: "1:687136905352:web:4f7be89300d651d3e5a73d",
+  measurementId: "G-8HMM27Y3MK"
+ };
+ 
+ const app = initializeApp(firebaseConfig);
+ const db = getFirestore(app);
+ const auth = getAuth(app);  // ← 認証機能を使うのに必要
+ 
+ export { db, auth };
