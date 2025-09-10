fb7690802bc5e8dddce2f073431489ae368ffb62
kohiraba rento <happyowl0825@icloud.com>
2025-09-10 23:39:55 +0900

    ヘッダー

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,28 @@
-export function Header() {
-    return (
-    <header>
-      <div className = "header-title">Reizouko</div>
+import { Link, useNavigate } from "react-router-dom";
+import "../css/Header.css";
+
+export const Header = () => {
+  const navigate = useNavigate();
+  return (
+    <header className="header">
+      <Link to="/" className="logo">
+        My App
+      </Link>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <Link to="/mypage">ホーム</Link>
+          </li>
+          <li>
+            <Link to="/calendar">カレンダー</Link>
+          </li>
+          <li>
+            <button onClick={() => navigate("/signin")}>ログアウト</button>
+          </li>
+        </ul>
+      </nav>
     </header>
-    )
-}
+  );
+};
+
+// ...existing code...
