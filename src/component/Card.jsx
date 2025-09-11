724913e8d922a808226924eae265267ec78ea452
Kanta <kumicho441@gmail.com>
2025-09-11 23:37:52 +0900

    function_coloring_Trello

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,6 +1,15 @@
 export function Card({ card, handleDeleteCard }) {
+
+  //消費期限による色の変化
+  let cardClass = "card"
+  if(card.expirationDate <= 1){ // もし消費期限が1日以内ならば
+    cardClass += " red" // cardClassにredを追加する
+  }else if(card.dead <=3){
+    cardClass += " yellow" //cardClassにyellowを追加する
+  }
+
   return (
-    <div className="card">
+    <div className={cardClass}>
       <div className="text">{card.name}</div>
       <div className="number">個数：{card.amount}個</div>
       <div className="deadline">消費期限：あと{card.expirationDate}日</div>
